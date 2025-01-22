8bd11cb36d195dafe976cc8af96c6fe05159bdb2
Vaibhav Barala <vaibhavbarala8@gmail.com>
2025-01-22 19:55:48 +0530

    latest6

--- Router/UserRouter.js
+++ Router/UserRouter.js
@@ -111,7 +111,7 @@ User_Router.get("/oauth2callback", async (req, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
       });
 
-       return res.redirect(`https://cold-weld.vercel.app/?user=${JSON.stringify(Found_User)}`)
+       return res.redirect(`https://cold-weld.vercel.app?user=${JSON.stringify(Found_User)}`)
      
     } else {
       // Create a new user if not found
