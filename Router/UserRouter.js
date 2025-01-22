e8169049893f2193e8886b2a3f01d75c33afb2cc
Vaibhav Barala <vaibhavbarala8@gmail.com>
2025-01-22 20:21:52 +0530

    latest7

--- Router/UserRouter.js
+++ Router/UserRouter.js
@@ -108,6 +108,7 @@ User_Router.get("/oauth2callback", async (req, res) => {
       res.cookie("user_token", userToken, {
         httpOnly: true,
         signed: true,
+        secure:true,
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
       });
 
@@ -130,6 +131,7 @@ User_Router.get("/oauth2callback", async (req, res) => {
       res.cookie("user_token", userToken, {
         httpOnly: true,
         signed: true,
+        secure:true,
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
       });
 return res.redirect(`https://cold-weld.vercel.app?user=${JSON.stringify(Saved_user)}`)
