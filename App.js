77e5b76080eaf67b7b0be61ac9304be29d1d0055
Vaibhav Barala <vaibhavbarala8@gmail.com>
2025-01-22 19:16:19 +0530

    latest3

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express()
 app.use(cookiParser("HELLOBHAI"))
 app.use(cors({
-    origin:["http://localhost:5173", "https://3894-2401-4900-7401-b311-38d7-4d3e-a8a7-6f53.ngrok-free.app/" , "https://cold-8uu31jkw0-vaibhavbarala26s-projects.vercel.app"],
+    origin:["http://localhost:5173", "https://cold-gqqcfrkqu-vaibhavbarala26s-projects.vercel.app/"],
     credentials:true,
 }))
 const PORT = 1042
