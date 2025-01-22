fd32e97fcfc6302db766a59cf8db9c5240c27719
Vaibhav Barala <vaibhavbarala8@gmail.com>
2025-01-22 20:13:22 +0530

    latest6.3

--- Configuration/GoogleAPI.js
+++ Configuration/GoogleAPI.js
@@ -10,7 +10,7 @@ const SCOPES = [
 const OAuth2Client = new google.auth.OAuth2(
    process.env.GOOGLE_SECRET_ID, // This should hold the Client ID
   process.env.GOOGLE_CLIENT_KEY , // This should hold the Client Secret
-  "http://localhost:1042/user/oauth2callback" // Redirect URI for local development
+  "https://cold-server-bj3d.vercel.app/user/oauth2callback" // Redirect URI for local development
 );
 
 module.exports = { OAuth2Client, SCOPES };
